dc3cc3c757d8fa20e251230c955b7c83827c3885
Domingo Ilarreta <domingo.ilarreta@gmail.com>
2021-05-29 22:10:18 -0400

    Cambios

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -4,9 +4,9 @@ import './navbar.styles.scss';
 
 const NavBar = () => (
     <div className="navbar">
-        <Link className="option" to="/">Home</Link>
-        <Link className="option option-last" to="/evil-group">Evil Group</Link>
-        <Link className="option option-last" to="/x-men">X-men</Link>
+        <Link className="option" to="/">All</Link>
+        <Link className="option" to="/evil-group">Evil Group</Link>
+        <Link className="option" to="/x-men">X-men</Link>
         <Link className="option option-last" to="/episodes">Episodes</Link>
     </div>
 );
